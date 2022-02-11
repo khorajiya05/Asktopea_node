36cd8a1cc003f795bc2f4acc17555cb29d179227
Juned <junedkhorajiya5@gmail.com>
2022-02-11 10:22:52 +0530

    Initial commit

---
+++ controller/session-controller.js
@@ -0,0 +1,15 @@
+const fs = require("fs")
+
+function login(req,res){
+    res.write("Login")
+    res.end()
+}
+
+function signup(req,res){
+    let signupHtml = fs.readFileSync("./views/Signup.html")
+    res.write(signupHtml)
+    res.end()
+}
+
+module.exports.login = login
+module.exports.signup = signup
